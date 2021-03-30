5234e75704a8d4bdb4c57a7e66b9ae4efaa72135
maksim <shulham89@mail.ru>
2021-03-30 22:03:44 +0300

    fixed error with empty callback

--- src/components/Common/SpongeBtn/SpongeBtn.jsx
+++ src/components/Common/SpongeBtn/SpongeBtn.jsx
@@ -2,8 +2,9 @@ import React,{useCallback} from 'react'
 import styles from './SpongeBtn.module.css'
 
 const SpongeBtn = ({className,children,onClick}) => {
-    const action = useCallback((cb)=>{
-        onClick();
+    const action = useCallback(()=>{
+        if(onClick)
+            onClick();
     },[onClick]);
 
     return (
